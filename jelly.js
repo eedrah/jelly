05504fcfe72b1e820c61084895c4f982bd3a67d8
Eedrah <9520856+eedrah@users.noreply.github.com>
2025-08-14 15:18:43 +1200

    remove unneeded demo code

--- jelly.js
+++ jelly.js
@@ -6,7 +6,6 @@ import {
 const demosSection = document.getElementById('demos')
 
 let faceDetector //: FaceDetector;
-let runningMode /*: string */ = 'IMAGE'
 
 // Initialize the object detector
 const initializefaceDetector = async () => {
@@ -18,124 +17,10 @@ const initializefaceDetector = async () => {
       modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
       delegate: 'GPU',
     },
-    runningMode: runningMode,
+    runningMode: 'VIDEO',
   })
-  demosSection.classList.remove('invisible')
 }
 initializefaceDetector()
-
-/********************************************************************
- // Demo 1: Grab a bunch of images from the page and detection them
- // upon click.
- ********************************************************************/
-
-const imageContainers = document.getElementsByClassName('detectOnClick')
-
-for (let imageContainer of imageContainers) {
-  imageContainer.children[0].addEventListener('click', handleClick)
-}
-
-/**
- * Detect faces in still images on click
- */
-async function handleClick(event) {
-  const highlighters =
-    event.target.parentNode.getElementsByClassName('highlighter')
-  while (highlighters[0]) {
-    highlighters[0].parentNode.removeChild(highlighters[0])
-  }
-
-  const infos = event.target.parentNode.getElementsByClassName('info')
-  while (infos[0]) {
-    infos[0].parentNode.removeChild(infos[0])
-  }
-  const keyPoints = event.target.parentNode.getElementsByClassName('key-point')
-  while (keyPoints[0]) {
-    keyPoints[0].parentNode.removeChild(keyPoints[0])
-  }
-
-  if (!faceDetector) {
-    console.log('Wait for objectDetector to load before clicking')
-    return
-  }
-
-  // if video mode is initialized, set runningMode to image
-  if (runningMode === 'VIDEO') {
-    runningMode = 'IMAGE'
-    await faceDetector.setOptions({ runningMode: 'IMAGE' })
-  }
-
-  const ratio = event.target.height / event.target.naturalHeight
-
-  // faceDetector.detect returns a promise which, when resolved, is an array of Detection faces
-  const detections = faceDetector.detect(event.target).detections
-  console.log(detections)
-
-  displayImageDetections(detections, event.target)
-}
-
-function displayImageDetections(
-  detections /*: [] */,
-  resultElement /* : HTMLElement */
-) {
-  const ratio = resultElement.height / resultElement.naturalHeight
-  console.log(ratio)
-
-  for (let detection of detections) {
-    // Description text
-    const p = document.createElement('p')
-    p.setAttribute('class', 'info')
-    p.innerText =
-      'Confidence: ' +
-      Math.round(parseFloat(detection.categories[0].score) * 100) +
-      '% .'
-    // Positioned at the top left of the bounding box.
-    // Height is whatever the text takes up.
-    // Width subtracts text padding in CSS so fits perfectly.
-    p.style =
-      'left: ' +
-      detection.boundingBox.originX * ratio +
-      'px;' +
-      'top: ' +
-      (detection.boundingBox.originY * ratio - 30) +
-      'px; ' +
-      'width: ' +
-      (detection.boundingBox.width * ratio - 10) +
-      'px;' +
-      'hight: ' +
-      20 +
-      'px;'
-    const highlighter = document.createElement('div')
-    highlighter.setAttribute('class', 'highlighter')
-    highlighter.style =
-      'left: ' +
-      detection.boundingBox.originX * ratio +
-      'px;' +
-      'top: ' +
-      detection.boundingBox.originY * ratio +
-      'px;' +
-      'width: ' +
-      detection.boundingBox.width * ratio +
-      'px;' +
-      'height: ' +
-      detection.boundingBox.height * ratio +
-      'px;'
-
-    resultElement.parentNode.appendChild(highlighter)
-    resultElement.parentNode.appendChild(p)
-    for (let keypoint of detection.keypoints) {
-      const keypointEl = document.createElement('spam')
-      keypointEl.className = 'key-point'
-      keypointEl.style.top = `${keypoint.y * resultElement.height - 3}px`
-      keypointEl.style.left = `${keypoint.x * resultElement.width - 3}px`
-      resultElement.parentNode.appendChild(keypointEl)
-    }
-  }
-}
-
-/********************************************************************
- // Demo 2: Continuously grab image from webcam stream and detect it.
- ********************************************************************/
 
 let video = document.getElementById('webcam')
 const liveView = document.getElementById('liveView')
@@ -167,14 +52,11 @@ async function enableCam(event) {
   // Hide the button.
   enableWebcamButton.classList.add('removed')
 
-  // getUsermedia parameters
-  const constraints = {
-    video: true,
-  }
-
   // Activate the webcam stream.
   navigator.mediaDevices
-    .getUserMedia(constraints)
+    .getUserMedia({
+      video: true,
+    })
     .then(function (stream) {
       video.srcObject = stream
       video.addEventListener('loadeddata', predictWebcam)
@@ -186,11 +68,6 @@ async function enableCam(event) {
 
 let lastVideoTime = -1
 async function predictWebcam() {
-  // if image mode is initialized, create a new classifier with video runningMode
-  if (runningMode === 'IMAGE') {
-    runningMode = 'VIDEO'
-    await faceDetector.setOptions({ runningMode: 'VIDEO' })
-  }
   let startTimeMs = performance.now()
 
   // Detect faces using detectForVideo
